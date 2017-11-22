dab678bae7cab0612449155eb03ca0dba4a8598c
Claire Murray <claire.e.murray00@gmail.com>
2017-11-22 17:34:30 -0500

    initializing property manager app

---
+++ db/migrate/20171122221033_change_column_data_type_to_integer.rb
@@ -0,0 +1,5 @@
+class ChangeColumnDataTypeToInteger < ActiveRecord::Migration[5.1]
+  def change
+    change_column :properties, :rent, :integer
+  end
+end
---
+++ db/migrate/20171122222056_remove_column_tracking_rent.rb
@@ -0,0 +1,5 @@
+class RemoveColumnTrackingRent < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :properties, :rent
+  end
+end
